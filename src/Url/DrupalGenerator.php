d480d8cfbadb06055fc771f658f10a50b69e23b9
Dave Reid <reid.dave@principal.com>
2023-05-08 10:05:16 -0500

    Adding DrupalGenerator for Drupal.org Gitlab projects.

---
+++ src/Url/DrupalGenerator.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace IonBazan\ComposerDiff\Url;
+
+use Composer\Package\PackageInterface;
+
+class DrupalGenerator extends GitlabGenerator
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsPackage(PackageInterface $package)
+    {
+        return $package->getName() === 'drupal/core' || parent::supportsPackage($package);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReleaseUrl(PackageInterface $package)
+    {
+        $name = $this->getDrupalProjectName($package);
+        $version = $package->getPrettyVersion();
+
+        if ($package->isDev()) {
+            return null;
+        }
+//// Always move dev-branchname to branchname-dev
+//if ($package->isDev() && substr($version, 0, 4) === 'dev-' && substr($version, -4) !== '-dev') {
+//    $version = substr($version, 4) . '-dev';
+//}
+
+        return sprintf('https://www.drupal.org/project/%s/releases/%s', $name, $version);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProjectUrl(PackageInterface $package)
+    {
+        $name = $this->getDrupalProjectName($package);
+        return sprintf('https://www.drupal.org/project/%s', $name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDomain()
+    {
+        return 'git.drupalcode.org';
+    }
+
+    protected function getDrupalProjectName(PackageInterface $package)
+    {
+        list(, $name) = explode('/', $package->getName(), 2);
+
+        // Special handling for drupal/core only.
+        if ($name === 'core')
+        {
+            $name = 'drupal';
+        }
+
+        return $name;
+    }
+}
+
+
