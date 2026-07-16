e40a4fe4152d5cb780bfc2e2a00de5c1949ff713
Ion Bazan <ion.bazan@gmail.com>
2026-07-17 00:52:13 +0800

    cleanup

--- src/Diff/DiffEntries.php
+++ src/Diff/DiffEntries.php
@@ -9,11 +9,4 @@ use ArrayIterator;
  */
 class DiffEntries extends ArrayIterator
 {
-    /**
-     * @param array<int, DiffEntry> $array
-     */
-    public function __construct(array $array = [], int $flags = 0)
-    {
-        parent::__construct($array, $flags);
-    }
 }
