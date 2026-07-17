4460bac425e55d958f5e2751efa58b1e29b9bc03
Ion Bazan <ion.bazan@gmail.com>
2026-07-17 18:46:10 +0800

    Add --sort option to order packages by name or operation type
    Closes #48

--- src/Diff/DiffEntries.php
+++ src/Diff/DiffEntries.php
@@ -30,4 +30,39 @@ class DiffEntries extends ArrayIterator
 
         return new self($filtered);
     }
+
+    /**
+     * Returns a new collection sorted by package name or operation type.
+     *
+     * @param string $by 'name' or 'operation'
+     */
+    public function sorted(string $by = 'name'): self
+    {
+        $entries = $this->getArrayCopy();
+
+        if ('operation' === $by) {
+            $order = [
+                DiffEntry::TYPE_INSTALL => 0,
+                DiffEntry::TYPE_UPGRADE => 1,
+                DiffEntry::TYPE_DOWNGRADE => 2,
+                DiffEntry::TYPE_CHANGE => 3,
+                DiffEntry::TYPE_REMOVE => 4,
+            ];
+
+            usort($entries, static function (DiffEntry $a, DiffEntry $b) use ($order): int {
+                $cmp = ($order[$a->getType()] ?? 99) - ($order[$b->getType()] ?? 99);
+                if (0 !== $cmp) {
+                    return $cmp;
+                }
+
+                return strcmp($a->getPackageName(), $b->getPackageName());
+            });
+        } else {
+            usort($entries, static function (DiffEntry $a, DiffEntry $b): int {
+                return strcmp($a->getPackageName(), $b->getPackageName());
+            });
+        }
+
+        return new self($entries);
+    }
 }
